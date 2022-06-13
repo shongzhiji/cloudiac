c3a70459f2d02a382be7a32624db98a99ecbafd2
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 07:54:19 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,11 +5,29 @@ terraform {
         source = "aliyun/alicloud"
         version = "1.99.0"
       }
+      nginx = {
+        source = "getstackhead/nginx"
+        version = "1.3.2"
+      }
+        linux = {
+        source = "TelkomIndonesia/linux"
+        version = "0.2.3"
+      }
+        mysql = {
+        source = "ZobairQ/mysql"
+        version = "1.11.2"
+      }
     }
  }
+  provider "linux" {
+    # Configuration options
+  }
 provider "alicloud" {
-    access_key = "LTAI5tKBBkjMi3uRqSkjsuBi"
-         secret_key = "ugRlX1VaN0gFgorGtVLqeD4OYxdCfJ"
-         region     ="cn-hangzhou"
+    region = "cn-hangzhou"
 }
-~   
+ provider "nginx" {
+    # Configuration options
+  }   
+ provider "mysql" {
+    # Configuration options
+  }
