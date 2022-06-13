9f2488a6b20824ff615171441e64fe644efd251a
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 07:45:13 +0800

    Create main.tf
    1

---
+++ main.tf
@@ -0,0 +1,15 @@
+
+terraform {
+    required_providers {
+      alicloud = {
+        source = "aliyun/alicloud"
+        version = "1.99.0"
+      }
+    }
+ }
+provider "alicloud" {
+    access_key = "LTAI5tKBBkjMi3uRqSkjsuBi"
+         secret_key = "ugRlX1VaN0gFgorGtVLqeD4OYxdCfJ"
+         region     ="cn-hangzhou"
+}
+~   
