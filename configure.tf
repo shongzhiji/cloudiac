dadb1ba91e53e50dd66d7af59d25860e5ffd55d2
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 08:50:22 +0800

    Create configure.tf

---
+++ configure.tf
@@ -0,0 +1,26 @@
+resource "alicloud_vpc" "vpc" {
+        name          = "tf_vpc"
+        cidr_block  = "172.16.0.0/16"
+}
+
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = alicloud_vpc.vpc.id
+  cidr_block        = "172.16.0.0/21"
+  availability_zone = "cn-hangzhou-i"
+}
+resource "alicloud_security_group" "default" {
+  name = "default"
+  vpc_id = alicloud_vpc.vpc.id
+}
+
+resource "alicloud_security_group_rule" "allow_all_tcp" {
+  type              = "ingress"
+  ip_protocol       = "tcp"
+  nic_type          = "intranet"
+  policy            = "accept"
+  port_range        = "1/65535"
+  priority          = 1
+  security_group_id = alicloud_security_group.default.id
+  cidr_ip           = "0.0.0.0/0"
+}
+
