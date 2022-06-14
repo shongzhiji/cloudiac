e0194d2318dbf6a175df9d2a2c89c7c10690037e
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 09:27:59 +0800

    Update configure.tf

--- configure.tf
+++ configure.tf
@@ -1,54 +1,11 @@
-variable "name" {
-  default = "adbClusterconfig"
+
+provider "mysql" {
+  endpoint = "my-database.example.com:3306"
+  username = "app-user"
+  password = "app-password"
 }
 
-variable "creation" {
-  default = "ADB"
-}
-
-data "alicloud_zones" "default" {
-  available_resource_creation = "cn-hangzhou-i"
-}
-
-resource "alicloud_vpc" "vpc" {
-        name          = "tf_vpc"
-        cidr_block  = "172.16.0.0/16"
-}
-
-resource "alicloud_vswitch" "vsw" {
-  vpc_id            = alicloud_vpc.vpc.id
-  cidr_block        = "172.16.0.0/21"
-  availability_zone = "cn-hangzhou-i"
-}
-resource "alicloud_security_group" "default" {
-  name = "default"
-  vpc_id = alicloud_vpc.vpc.id
-}
-
-resource "alicloud_security_group_rule" "allow_all_tcp" {
-  type              = "ingress"
-  ip_protocol       = "tcp"
-  nic_type          = "intranet"
-  policy            = "accept"
-  port_range        = "1/65535"
-  priority          = 1
-  security_group_id = alicloud_security_group.default.id
-  cidr_ip           = "0.0.0.0/0"
-}
-resource "alicloud_adb_cluster" "cluster" {
-  db_cluster_version  = "3.0"
-  db_cluster_category = "Cluster"
-  db_node_class       = "C8"
-  db_node_count       = 2
-  db_node_storage     = 200
-  pay_type            = "PostPaid"
-  vswitch_id          = alicloud_vswitch.vsw.id
-  description         = var.name
-}
-
-resource "alicloud_adb_account" "account" {
-  db_cluster_id       = alicloud_adb_cluster.cluster.id
-  account_name        = "tftestnormal"
-  account_password    = "Test12345"
-  account_description = var.name
+# Create a Database
+resource "mysql_database" "app" {
+  name = "my_awesome_app"
 }
