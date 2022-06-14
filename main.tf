add6c011ddc51c75ad95dfdbf0262b5d983c88bd
shongzhiji <83902769+shongzhiji@users.noreply.github.com>
2022-06-14 09:51:46 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,22 @@
 
-terraform {
+ terraform {
     required_providers {
-      nginx = {
-        source = "getstackhead/nginx"
-        version = "1.3.2"
+      alicloud = {
+        source = "aliyun/alicloud"
+        version = "1.99.0"
       }
-        linux = {
+          linux = {
         source = "TelkomIndonesia/linux"
         version = "0.2.3"
       }
-
-    mysql = {
+        nginx = {
+        source = "getstackhead/nginx"
+        version = "1.3.2"
+      }
+          mysql = {
       source = "ZobairQ/mysql"
       version = "1.11.4"
     }
-
     }
- }
+  }
+      
